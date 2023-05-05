07156a4927547e15180e010f3eb7808c2d6a6760
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-05 10:58:22 -0700

    WIP: working on UI logic

--- Baker/Program.cs
+++ Baker/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Baker.UserInterfaceModels;
 using Baker.Models;
 
 namespace Baker
@@ -7,7 +8,68 @@ namespace Baker
   {
     static void Main()
     {
+      Console.WriteLine("Welcome to...");
+      Console.WriteLine($"{Banner.Welcome}");
+      Console.WriteLine("Place an order? (y/n)");
+      string input = Console.ReadLine();
+      if (input == "y")
+      {
+        Console.WriteLine("What sounds good?");
+        Console.WriteLine("Bread");
+        Console.WriteLine("Pastry");
+        string choice = Console.ReadLine().ToLower();
+        if (choice == "bread")
+        {
+          Console.WriteLine($"Your total comes to: ${BreadOrder()}");
+        }
+        else if (choice == "pastry")
+        {
+          Console.WriteLine($"Your total comes to ${PastryOrder()}");
+        }
+        else
+        {
+          Console.WriteLine("Please choose from the listed options");
+        }
+      }
+      // else
+      // {
+      //   Console.WriteLine("Goodbye!");
+      // }
+      // static void PlaceOrder()
+      // {
+      // Console.WriteLine("What sounds good?");
+      // Console.WriteLine("Bread");
+      // Console.WriteLine("Pastry");
+      // string choice = Console.ReadLine().ToLower();
+      // if(choice == "bread")
+      // {
+      //   Console.WriteLine($"Your total comes to: ${BreadOrder()}");
+      // }
+      // else if(choice == "pastry")
+      // {
+      // PastryOrder();
+      // }
+      // else
+      // {
+      //   Console.WriteLine("Please choose from the listed options");
+      //   PlaceOrder();
 
+      // }
+      // }
+      static int BreadOrder()
+      {
+        Console.WriteLine("How many loaves would you like?");
+        int breadOrder = int.Parse(Console.ReadLine());
+        Bread newBreadOrder = new Bread(breadOrder);
+        return newBreadOrder.CalculateTotal();
+      }
+      static int PastryOrder()
+      {
+        Console.WriteLine("How many pastries would you like?");
+        int pastryOrder = int.Parse(Console.ReadLine());
+        Pastry newPastryOrder = new Pastry(pastryOrder);
+        return newPastryOrder.CalculateTotal();
+      }
     }
   }
 }
