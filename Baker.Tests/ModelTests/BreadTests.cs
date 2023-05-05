cb1e56292b5190c62dc87390ac91524e37c25b04
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-05 09:07:27 -0700

    add loafPrice and orderAmount private fields and constructor to Bread class + passing test

--- Baker.Tests/ModelTests/BreadTests.cs
+++ Baker.Tests/ModelTests/BreadTests.cs
@@ -1,16 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Baker.Models;
 using System;
 
 namespace Baker.Tests
 {
   [TestClass]
-  public class Bread
+  public class BreadTests
   {
     [TestMethod]
     public void Bread_CreatesInstanceOfBread_Bread()
     {
-      Bread breadOrder = new Bread();
+      Bread breadOrder = new Bread(4);
       Assert.AreEqual(typeof(Bread), breadOrder.GetType());
+    }
+    [TestMethod]
+    public void Bread_CreatesBreadWithPriceAndNumberOfLoaves_Bread()
+    {
+      int orderAmount = 4;
+      int expected = 4;
+      Bread breadOrder = new Bread(orderAmount);
+      Assert.AreEqual(expected, breadOrder.GetOrderAmount());
     }
   }
 }
--- Baker/Models/Bread.cs
+++ Baker/Models/Bread.cs
@@ -2,6 +2,15 @@ namespace Baker.Models
 {
   public class Bread
   {
-
+    private int _loafPrice = 2;
+    private int _orderAmount;
+    public Bread(int orderAmount)
+    {
+      _orderAmount = orderAmount;
+    }
+    public int GetOrderAmount()
+    {
+      return _orderAmount;
+    }
   }
 }
