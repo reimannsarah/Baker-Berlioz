0a5d8ade3b3ed76b560912c2d9e6ec9a7630a10d
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-05 10:09:42 -0700

    add getter methods for private fields and GetOrderAmount method  to Pastry class + passing test

--- Baker.Tests/ModelTests/PastryTests.cs
+++ Baker.Tests/ModelTests/PastryTests.cs
@@ -9,8 +9,15 @@ namespace Baker.Tests
     [TestMethod]
     public void Pastry_CreatesInstanceOfPastry_Pastry()
     {
-      Pastry pastryOrder = new Pastry();
+      Pastry pastryOrder = new Pastry(4);
       Assert.AreEqual(typeof(Pastry), pastryOrder.GetType());
+    }
+    [TestMethod]
+    public void Pastry_CreatesPastryWithPriceAndAmount_Pastry()
+    {
+      int expected = 4;
+      Pastry pastryOrder = new Pastry(4);
+      Assert.AreEqual(expected, pastryOrder.GetOrderAmount());
     }
   }
 }
--- Baker/Models/Pastry.cs
+++ Baker/Models/Pastry.cs
@@ -2,6 +2,19 @@ namespace Baker
 {
   public class Pastry
   {
-
+    private int _pastryPrice = 2;
+    private int _orderAmount;
+    public Pastry(int orderAmount)
+    {
+      _orderAmount = orderAmount;
+    }
+    public int GetPastryPrice()
+    {
+      return _pastryPrice;
+    }
+    public int GetOrderAmount()
+    {
+      return _orderAmount;
+    }
   }
 }
