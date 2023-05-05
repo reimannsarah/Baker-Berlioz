d7dcd335d7b04f7d09dab2b8db077da1c491c98c
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-05 15:48:23 -0700

    create getter and setter properties for OrderAmount property

--- Baker/Models/Pastry.cs
+++ Baker/Models/Pastry.cs
@@ -4,6 +4,16 @@ namespace Baker
   {
     private int _pastryPrice = 2;
     private int _orderAmount;
+    public int OrderAmount 
+    {
+      get { return _orderAmount; }
+      set { _orderAmount = value; }
+    }
+    public int Total = 0;
+    public Pastry()
+    {
+
+    }
     public Pastry(int orderAmount)
     {
       _orderAmount = orderAmount;
@@ -18,19 +28,19 @@ namespace Baker
     }
     public int CalculateTotal()
     {
-      int total = 0;
+      // int total = 0;
       for(int i = 1; i <= _orderAmount; i++)
       {
         if(i % 4 != 0)
         {
-          total += 2;
+          Total += 2;
         }
         else
         {
-          total += 0;
+          Total += 0;
         }
       }
-      return total;
+      return Total;
     }
   }
 }
